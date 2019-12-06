70ff283371b1d35a94d446e4d3a72f92ce71ea4d
alberto56 <albert@mediatribe.net>
2019-12-06 12:48:46 -0500

    Please squash this commit in the pull request

--- scripts/test-running-environment.sh
+++ scripts/test-running-environment.sh
@@ -8,10 +8,18 @@ echo '=> Running tests on a running environment.'
 URL="$(docker-compose port drupal 80)"
 TOKEN="$(docker-compose exec drupal /bin/bash -c 'drush ev "expose_status_token()"')"
 
+echo 'Make sure values returned make sense with base and submodules enabled'
 curl "$URL/admin/reports/status/expose/not-the-right-token" | grep 'Access denied'
 curl "$URL/admin/reports/status/expose/$TOKEN" | grep '"status":"issues found; please check"'
 
 docker-compose exec drupal /bin/bash -c 'drush pmu -y expose_status_details expose_status_ignore expose_status_severity'
 
+echo 'Make sure values returned make sense with only the base module enabled'
 curl "$URL/admin/reports/status/expose/not-the-right-token" | grep 'Access denied'
 curl "$URL/admin/reports/status/expose/$TOKEN" | grep '"status":"issues found; please check"'
+
+docker-compose exec drupal /bin/bash -c 'drush sset expose_status_token some-token'
+echo 'Make sure it is possible to uninstall the module'
+docker-compose exec drupal /bin/bash -c 'drush pmu -y expose_status'
+echo 'Once module is uninstalled, state variable should be deleted'
+docker-compose exec drupal /bin/bash -c 'drush sget expose_status_token' | grep -v some-token
