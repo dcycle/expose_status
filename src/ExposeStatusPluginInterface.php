0efc334b505ceacf76ae0254cbbf693c1e6ffccc
alberto56 <albert@mediatribe.net>
2020-02-04 14:30:52 -0500

    Remove obsolete comments from code copy-pasted from the Examples module

--- src/ExposeStatusPluginInterface.php
+++ src/ExposeStatusPluginInterface.php
@@ -5,21 +5,9 @@ namespace Drupal\expose_status;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- * An interface for all Sandwich type plugins.
+ * An interface for all ExposeStatusPlugin type plugins.
  *
- * When defining a new plugin type you need to define an interface that all
- * plugins of the new type will implement. This ensures that consumers of the
- * plugin type have a consistent way of accessing the plugin's functionality. It
- * should include access to any public properties, and methods for accomplishing
- * whatever business logic anyone accessing the plugin might want to use.
- *
- * For example, an image manipulation plugin might have a "process" method that
- * takes a known input, probably an image file, and returns the processed
- * version of the file.
- *
- * In our case we'll define methods for accessing the human readable description
- * of a sandwich and the number of calories per serving. As well as a method for
- * ordering a sandwich.
+ * This is based on code from the Examples module.
  */
 interface ExposeStatusPluginInterface {
 
