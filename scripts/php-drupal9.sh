5dd101938cec6cc45a32c00acaaf4cd31cd2ea81
alberto56 <albert@mediatribe.net>
2020-02-29 19:30:04 -0600

    Workaround for drupal-check tty error

--- scripts/php-drupal9.sh
+++ scripts/php-drupal9.sh
@@ -5,4 +5,6 @@
 set -e
 
 echo "=> Identify deprecated code so we're ready for Drupal 9"
-docker run --rm -v "$(pwd)":/var/www/html/modules/expose_status dcycle/drupal-check:1 expose_status/src
+# See https://github.com/dcycle/docker-drupal-check on why we're using
+# this weird tag
+docker run --rm -v "$(pwd)":/var/www/html/modules/expose_status dcycle/drupal-check:1.2019-12-30-21-59-43-UTC expose_status/src
