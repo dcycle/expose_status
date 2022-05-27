8df1649c8c515856afb303f360c60de133d9e124
Albert Albala <albert@mediatribe.net>
2022-05-26 21:52:12 -0400

    Upgrade Docker on DigitalOcean Docker VM (#17)

--- scripts/jenkins/periodic-test.sh
+++ scripts/jenkins/periodic-test.sh
@@ -5,7 +5,7 @@ source ~/.docker-host-ssh-credentials
 # Create a droplet
 DROPLET_NAME=expose-status
 IP1=$(ssh "$DOCKERHOSTUSER@$DOCKERHOST" \
-  "./digitalocean/scripts/new-droplet.sh "$DROPLET_NAME)
+  "./digitalocean/scripts/new-droplet.sh -aubuntu "$DROPLET_NAME)
 # https://github.com/dcycle/docker-digitalocean-php#public-vs-private-ip-addresses
 IP2=$(ssh "$DOCKERHOSTUSER@$DOCKERHOST" "./digitalocean/scripts/list-droplets.sh" |grep "$IP1" --after-context=10|tail -1|cut -b 44-)
 echo "Now determining which of the IPs $IP1 or $IP2 is the public IP"
@@ -21,4 +21,4 @@ ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
   root@"$IP" \
   "git clone http://github.com/dcycle/expose_status && \
   cd expose_status && \
-  ./scripts/ci.sh"
+  ./scripts/install-docker-and-ci.sh"
