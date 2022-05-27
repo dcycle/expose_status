fb46550e6ffd2a01332e23ec6d61cf0f97123ca5
Albert Albala <albert@mediatribe.net>
2022-05-26 21:44:14 -0400

    Please squash this commit in the pull request

--- scripts/jenkins/periodic-test.sh
+++ scripts/jenkins/periodic-test.sh
@@ -5,7 +5,7 @@ source ~/.docker-host-ssh-credentials
 # Create a droplet
 DROPLET_NAME=expose-status
 IP1=$(ssh "$DOCKERHOSTUSER@$DOCKERHOST" \
-  "./digitalocean/scripts/new-droplet.sh "$DROPLET_NAME)
+  "./digitalocean/scripts/new-droplet.sh -aubuntu "$DROPLET_NAME)
 # https://github.com/dcycle/docker-digitalocean-php#public-vs-private-ip-addresses
 IP2=$(ssh "$DOCKERHOSTUSER@$DOCKERHOST" "./digitalocean/scripts/list-droplets.sh" |grep "$IP1" --after-context=10|tail -1|cut -b 44-)
 echo "Now determining which of the IPs $IP1 or $IP2 is the public IP"
@@ -19,6 +19,6 @@ sleep 90
 
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
   root@"$IP" \
-  "git clone http://github.com/dcycle/expose_status && \
+  "git clone --branch working http://github.com/dcycle/expose_status && \
   cd expose_status && \
-  ./scripts/ci.sh"
+  ./scripts/install-docker-and-ci.sh"
